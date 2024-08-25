55a5932586fd7bbafd309b373a77a5eb863d4731
Lu Xu <thusimon@gmail.com>
2024-08-25 15:37:12 -0700

    init commit

---
+++ YouTubeAssist/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YouTubeAssist.UI;
+
+namespace YouTubeAssist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SearchView searchView;
+        NotificationsView notificationsView;
+        AboutView aboutView;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            searchView = new SearchView();
+            notificationsView = new NotificationsView();
+            aboutView = new AboutView();
+            MainContentControl.Content = searchView;
+        }
+
+        private void Search_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = searchView;
+        }
+
+        private void Notifications_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = notificationsView;
+        }
+
+        private void About_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = aboutView;
+        }
+    }
+}
