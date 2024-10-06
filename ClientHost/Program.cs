f499f5a9034208713ba23f23f4ebef3e25fda00c
Lu Xu <thusimon@gmail.com>
2024-10-05 21:49:36 -0700

    cont

--- ClientHost/Program.cs
+++ ClientHost/Program.cs
@@ -1,19 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ClientHost;
+using System.Diagnostics;
 using System.IO.Pipes;
+using System.Reflection.PortableExecutable;
 using System.Security.Principal;
+using System.Text;
 
 
 var pipeClient = new NamedPipeClientStream(".", "com.utticus.youtube.assist",
                         PipeDirection.InOut, PipeOptions.None,
                         TokenImpersonationLevel.Impersonation);
 
-pipeClient.Connect();
-Console.WriteLine("Connected to server pipe.\n");
+//pipeClient.Connect();
+//Console.WriteLine("Connected to server pipe.\n");
 
-var ss = new StreamString(pipeClient);
-var sr = new StreamReader(pipeClient);
-var sw = new StreamWriter(pipeClient);
+//var ss = new StreamString(pipeClient);
+//var sr = new StreamReader(pipeClient);
+//var sw = new StreamWriter(pipeClient);
+//sw.AutoFlush = true;
 
 static async Task<string?> consoleReadLine()
 {
@@ -32,13 +36,16 @@ async Task writePipeStream(StreamString ss)
     }
 }
 
-async Task writePipeStream2(StreamWriter sw)
+async Task writePipeStream2(StreamWriter sw, StreamReader sr)
 {
     string? message = "";
-    while ((message = await consoleReadLine()) != "q")
-    {
-        sw.WriteLine(message!);
-    }
+    //while ((message = await consoleReadLine()) != "q")
+    //{
+    //    sw.WriteLine(message!);
+    //}
+    message = await consoleReadLine();
+    string responseFromServer = sr.ReadLine();
+    Console.WriteLine("Received from server: " + responseFromServer);
 }
 
 async Task readPipeStream(StreamString ss)
@@ -67,11 +74,166 @@ async Task hostTasks(StreamString ss)
 
 async Task hostTasks2(StreamReader sr, StreamWriter sw)
 {
-    await Task.Run(() => writePipeStream2(sw));
-    //await Task.Run(() => readPipeStream(ss));
+    await Task.Run(() => writePipeStream2(sw, sr));
+    //await Task.Run(() => readPipeStream2(sr));
 }
 
-sw.AutoFlush = true;
 Console.WriteLine("Running r/w PipeStream tasks");
 //await hostTasks2(sr, sw);
-await hostTasks(ss);
+//await hostTasks(ss);
+
+static void ReadData(object pipeClientObj)
+{
+    NamedPipeClientStream pipeClient = (NamedPipeClientStream)pipeClientObj;
+    while (true)
+    {
+        string input = Console.ReadLine();
+        byte[] data = Encoding.UTF8.GetBytes(input);
+        pipeClient.Write(data, 0, data.Length);
+    }
+
+    //byte[] buffer = new byte[1024];
+    //int bytesRead;
+
+    //while ((bytesRead = pipeClient.Read(buffer, 0, buffer.Length)) > 0)
+    //{
+    //    // Process the received data
+    //    Console.WriteLine("Received data from server: " + Encoding.UTF8.GetString(buffer, 0, bytesRead));
+    //}
+}
+
+static void WriteData(object pipeClientObj)
+{
+    NamedPipeClientStream pipeClient = (NamedPipeClientStream)pipeClientObj;
+
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+
+    while(true)
+    {
+        // Read a response from the server (optional)
+        bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received from server: " + response);
+    }
+
+    // Send data to the server
+    //string input = Console.ReadLine();
+    //byte[] data = Encoding.UTF8.GetBytes("Test");
+    //byte[] data = Encoding.UTF8.GetBytes(input);
+    //while (true)
+    //{
+    //    string input = Console.ReadLine();
+    //    writer.WriteLine(input);
+
+    //    // Read a response from the server (optional)
+    //    using (StreamReader reader = new StreamReader(pipeClient))
+    //    {
+    //        string response = reader.ReadLine();
+    //        Console.WriteLine("Received from server: " + response);
+    //    }
+    //}
+
+    //pipeClient.Write(data, 0, data.Length);
+    //pipeClient.Flush();
+}
+
+string? message = "";
+//while ((message = await consoleReadLine()) != "q")
+//{
+//    sw.WriteLine(message!);
+//}
+//message = await consoleReadLine();
+//string responseFromServer = sr.ReadLine();
+//Console.WriteLine("Received from server: " + responseFromServer);
+
+try
+{
+    Console.WriteLine("Connecting to server...");
+    pipeClient.Connect(5000);  // Timeout after 5 seconds if the server isn't available
+    Console.WriteLine("Connected to server.");
+
+    //using (StreamWriter writer = new StreamWriter(pipeClient, Encoding.UTF8) { AutoFlush = true })
+    //{
+    //    string messageToServer = "Hello from client!";
+    //    writer.WriteLine(messageToServer);  // Send message
+    //    writer.Flush();  // Ensure it is sent immediately
+    //    Console.WriteLine("Sent to server: " + messageToServer);
+    //}
+
+    //// Read response from server
+    //using (StreamReader reader = new StreamReader(pipeClient, Encoding.UTF8))
+    //{
+    //    string responseFromServer = reader.ReadLine();  // Read response
+    //    Console.WriteLine("Received from server: " + responseFromServer);
+    //}
+
+    //pipeClient.WaitForPipeDrain();
+
+    //string msg = "Hello from the client!";
+    //byte[] data = Encoding.UTF8.GetBytes(msg);
+    //pipeClient.Write(data, 0, data.Length);
+
+    //byte[] buffer = new byte[1024];
+    //int bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+    //string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+    //Console.WriteLine("Received response  from server: " + response);
+
+    //using (StreamWriter writer = new StreamWriter(pipeClient))
+    //{
+    //    writer.WriteLine("Hello from the client!");
+    //}
+
+    //using (StreamReader reader = new StreamReader(pipeClient))
+    //{
+    //    string data;
+
+    //    while ((data = reader.ReadLine()) != null)
+    //    {
+    //        // Process the received string
+    //        Console.WriteLine("Received response from server: " + data);
+    //    }
+    //}
+
+    //byte[] data = Encoding.UTF8.GetBytes("Hello from the client!");
+    //pipeClient.Write(data, 0, data.Length);
+
+    //byte[] buffer = new byte[1024];
+    //int bytesRead;
+
+    //while ((bytesRead = pipeClient.Read(buffer, 0, buffer.Length)) > 0)
+    //{
+    //    // Process the received bytes
+    //    Console.WriteLine("Received response from server: " + Encoding.UTF8.GetString(buffer, 0, bytesRead));
+    //}
+
+    //Thread readThread = new Thread(ReadData);
+    //readThread.Start(pipeClient);
+
+    //Thread writeThread = new Thread(WriteData);
+    //writeThread.Start(pipeClient);
+
+    //readThread.Join();
+    //writeThread.Join();
+
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+
+    while (true)
+    {
+        string input = Console.ReadLine();
+        byte[] data = Encoding.UTF8.GetBytes(input);
+        pipeClient.Write(data, 0, data.Length);
+
+        // Read a response from the server (optional)
+        bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received from server: " + response);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
--- YouTubeAssist/Services/PipeServer.cs
+++ YouTubeAssist/Services/PipeServer.cs
@@ -41,45 +41,134 @@ namespace YouTubeAssist.Services
             pipeServer.WaitForConnection();
 
             Debug.WriteLine("Client connected on thread[{0}].", threadId);
-            pipeView.MessageIncome += String.Format("\nClient connected on thread[{0}].\n", threadId);
+            pipeView.MessageIncome += String.Format("\nClient connected on thread [{0}] as user {1}\n", threadId, pipeServer.GetImpersonationUserName());
 
             try
             {
-                // Read the request from the client. Once the client has
-                // written to the pipe its security token will be available.
-
-                StreamString ss = new StreamString(pipeServer);
-                StreamString ss2 = new StreamString(pipeServer);
-                //StreamWriter sw = new StreamWriter(pipeServer);
-                //StreamReader sr = new StreamReader(pipeServer);
-                //sw.AutoFlush = true;
-
-                // Verify our identity to the connected client using a
-                // string that the client anticipates.
-
-                await serverTasks(ss, ss2, threadId, pipeServer.GetImpersonationUserName());
-
-                //await Task.Run(() => handlePipeStream2(sr, sw, threadId, pipeServer.GetImpersonationUserName()));
-                //await Task.Run(() => readPipeStream(ss));
-
-                //string message = "";
-                //while ((message = ss.ReadString()) != "!q")
+                // Read from client
+                //using (StreamReader reader = new StreamReader(pipeServer, Encoding.UTF8, false, 1024))
                 //{
-                //    // Display the name of the user we are impersonating.
-                //    Debug.WriteLine("Get message: {0} on thread[{1}] as user: {2}.",
-                //        message, threadId, pipeServer.GetImpersonationUserName());
-                //    ss.WriteString(String.Format("Forwarded from server pipe: {0}", message));
+                //    string messageFromClient = reader.ReadLine();  // Read client's message
+                //    Console.WriteLine("Received from client: " + messageFromClient);
                 //}
-                //Debug.WriteLine("Get quite message on thread[{0}] as user: {1}, will close the pipe.",
-                //        threadId, pipeServer.GetImpersonationUserName());
-                //pipeServer.Close();
+
+                //// Write response to client
+                //using (StreamWriter writer = new StreamWriter(pipeServer, Encoding.UTF8, 1024) { AutoFlush = true })
+                //{
+                //    string response = "Hello from server!";
+                //    writer.WriteLine(response);  // Send response
+                //    writer.Flush();  // Ensure response is sent
+                //    Console.WriteLine("Sent to client: " + response);
+                //}
+                //pipeServer.WaitForPipeDrain();
+
+                //byte[] buffer = new byte[1024];
+                //int bytesRead = pipeServer.Read(buffer, 0, buffer.Length);
+                //string d = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                //Debug.WriteLine("Received message from client: " + d);
+
+                //string response = "Hello from the server!";
+                //byte[] responseData = Encoding.UTF8.GetBytes(response);
+                //pipeServer.Write(responseData, 0, responseData.Length);
+
+                //using (StreamReader reader = new StreamReader(pipeServer))
+                //{
+                //    string d;
+
+                //    while ((d = reader.ReadLine()) != null)
+                //    {
+                //        // Process the received string
+                //        Debug.WriteLine("Received string: " + d);
+                //    }
+                //}
+
+                //// Send a response (optional)
+                //using (StreamWriter writer = new StreamWriter(pipeServer))
+                //{
+                //    writer.WriteLine("Hello from the server!");
+                //}
+
+                //byte[] buffer = new byte[1024];
+                //int bytesRead;
+
+                //while ((bytesRead = pipeServer.Read(buffer, 0, buffer.Length)) > 0)
+                //{
+                //    // Process the received bytes
+                //    Debug.WriteLine("Received bytes: " + Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                //}
+
+                //// Send a response (optional)
+                //byte[] responseData = Encoding.UTF8.GetBytes("Hello from the server!");
+                //pipeServer.Write(responseData, 0, responseData.Length);
+
+                //Thread readThread = new Thread(ReadData);
+                //readThread.Start(pipeServer);
+
+                //Thread writeThread = new Thread(WriteData);
+                //writeThread.Start(pipeServer);
+
+                //readThread.Join();
+                //writeThread.Join();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = pipeServer.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string input = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Debug.WriteLine("Received from client: " + input);
+
+                    // Send a response (optional)
+                    string response = "Received input: " + input;
+                    byte[] responseData = Encoding.UTF8.GetBytes(response);
+                    pipeServer.Write(responseData, 0, responseData.Length);
+                }
             }
-            // Catch the IOException that is raised if the pipe is broken or disconnected.
-            catch (IOException e)
+            catch (Exception e)
             {
                 Debug.WriteLine("ERROR: {0}", e.Message);
                 pipeServer.Close();
             }
+            
+
+            //try
+            //{
+            //    // Read the request from the client. Once the client has
+            //    // written to the pipe its security token will be available.
+
+            //    StreamString ss = new StreamString(pipeServer);
+            //    StreamString ss2 = new StreamString(pipeServer);
+            //    //StreamWriter sw = new StreamWriter(pipeServer);
+            //    //StreamReader sr = new StreamReader(pipeServer);
+            //    //sw.AutoFlush = true;
+
+            //    // Verify our identity to the connected client using a
+            //    // string that the client anticipates.
+
+            //    await serverTasks(ss, ss2, threadId, pipeServer.GetImpersonationUserName());
+
+            //    //await Task.Run(() => handlePipeStream2(sr, sw, threadId, pipeServer.GetImpersonationUserName()));
+            //    //await Task.Run(() => readPipeStream(ss));
+
+            //    //string message = "";
+            //    //while ((message = ss.ReadString()) != "!q")
+            //    //{
+            //    //    // Display the name of the user we are impersonating.
+            //    //    Debug.WriteLine("Get message: {0} on thread[{1}] as user: {2}.",
+            //    //        message, threadId, pipeServer.GetImpersonationUserName());
+            //    //    ss.WriteString(String.Format("Forwarded from server pipe: {0}", message));
+            //    //}
+            //    //Debug.WriteLine("Get quite message on thread[{0}] as user: {1}, will close the pipe.",
+            //    //        threadId, pipeServer.GetImpersonationUserName());
+            //    //pipeServer.Close();
+            //}
+            //// Catch the IOException that is raised if the pipe is broken or disconnected.
+            //catch (IOException e)
+            //{
+            //    Debug.WriteLine("ERROR: {0}", e.Message);
+            //    pipeServer.Close();
+            //}
         }
 
         void handlePipeStream(StreamString ss, StreamString ss2,int threadId, string impersonationUsername)
@@ -96,20 +185,28 @@ namespace YouTubeAssist.Services
             }
         }
 
-        void handlePipeStream2(StreamReader sr, StreamWriter sw, int threadId, string impersonationUsername)
+        void handlePipeStream2(StreamReader sr, StreamWriter sw)
         {
-            while (true)
-            {
-                string message = sr.ReadLine();
-                Debug.WriteLine("Get message: {0} on thread[{1}] as user: {2}.",
-                            message, threadId, impersonationUsername);
-                sw.WriteLine(String.Format("Forwarded from server pipe: {0}", message));
-            }
+            //while (true)
+            //{
+            //    string? message = sr.ReadLine();
+            //    Debug.WriteLine("Get message: {0}", message);
+            //    sw.WriteLine(String.Format("Forwarded from server pipe: {0}", message));
+            //}
+            string? message = sr.ReadLine();
+            Debug.WriteLine("Get message: {0}", message);
+            sw.WriteLine(String.Format("Forwarded from server pipe: {0}", message));
         }
 
         async Task serverTasks(StreamString ss, StreamString ss2, int threadId, string impersonationUsername)
         {
             await Task.Run(() => handlePipeStream(ss, ss2, threadId, impersonationUsername));
+            //await Task.Run(() => readPipeStream(ss));
+        }
+
+        async Task serverTasks2(StreamReader sr, StreamWriter sw)
+        {
+            await Task.Run(() => handlePipeStream2(sr, sw));
             //await Task.Run(() => readPipeStream(ss));
         }
 
@@ -119,5 +216,27 @@ namespace YouTubeAssist.Services
             Debug.WriteLine(message);
         }
 
+        static void ReadData(object pipeServerObj)
+        {
+            NamedPipeServerStream pipeServer = (NamedPipeServerStream)pipeServerObj;
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = pipeServer.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                // Process the received data
+                Debug.WriteLine("Received data: " + Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+        }
+
+        static void WriteData(object pipeServerObj)
+        {
+            NamedPipeServerStream pipeServer = (NamedPipeServerStream)pipeServerObj;
+
+            // Send data to the client
+            byte[] responseData = Encoding.UTF8.GetBytes("Hello from the server!");
+            pipeServer.Write(responseData, 0, responseData.Length);
+        }
     }
 }
